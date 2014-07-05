a98a85eff2e99c0e4c6488f64efc20afcb74c9a7
D_Money <mullinsdevin@ku.edu>
2014-07-05 18:10:55 -0500

    Quit your bitchin

--- game.cpp
+++ game.cpp
@@ -3,7 +3,7 @@
 
 Game::Game( bool is_server )
 {
-
+	this.is_server = is_server;
 }
 
 void Game::add_mission_vote( int player_id, bool success )
@@ -13,7 +13,20 @@ void Game::add_mission_vote( int player_id, bool success )
 
 void Game::add_player( int player_id )
 {
+	if (this.is_server)
+	{
+		this.player_list.insert(player_id);
+	}
+	else
+	{
+		//Tell server to add player
+	}
 
+}
+
+Player Game::get_player(int player_id)
+{
+	return this.player_list[player_id];
 }
 
 void Game::add_selection_vote( int player_id, bool success )
@@ -21,7 +34,7 @@ void Game::add_selection_vote( int player_id, bool success )
 
 }
 
-void Game::make_selection( int player_id, vector selection )
+void Game::make_selection( int player_id, vector<int> selection )
 {
 
 }
--- game.h
+++ game.h
@@ -14,10 +14,11 @@ public:
 	void add_selection_vote( int player_id, bool success );
 	int get_crown();
 	Player get_player( int player_id );
-	void make_selection( int player_id, vector selection );
+	void make_selection( int player_id, vector<int> selection );
 	void start();
 private:
 	bool is_server;		// Is this game instance a server?
+	vector<Player> player_list // List of players in the game
 };
 
 enum Role
