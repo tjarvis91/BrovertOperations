e158ce4c2383bf798c198d85a4c57af278d92a1e
JGrok <justinbaird@live.com>
2014-07-05 17:09:43 -0500

    Added stub game class

---
+++ game.cpp
@@ -0,0 +1,32 @@
+#include "game.h"
+#include <vector>
+
+Game::Game( bool is_server )
+{
+
+}
+
+void Game::add_mission_vote( int player_id, bool success )
+{
+
+}
+
+void Game::add_player( int player_id )
+{
+
+}
+
+void Game::add_selection_vote( int player_id, bool success )
+{
+
+}
+
+void Game::make_selection( int player_id, vector selection )
+{
+
+}
+
+void Game::start()
+{
+
+}
--- main.cpp
+++ main.cpp
@@ -5,6 +5,36 @@ int main()
 	// Initialise the 2D graphics system
 	Iw2DInit();
 
+	// Model test
+	Game game = Game( true );
+	
+	// Add players
+	game.add_player( 0 );
+	game.add_player( 1 );
+	game.add_player( 2 );
+	game.add_player( 3 );
+	game.add_player( 4 );
+
+	// Start the game
+	game.start();
+
+	// Get players
+	Player players[ 5 ];
+	for( int i=0; i<5; i++ )
+	{
+		players[ i ] = game.get_player( i );
+	}
+
+	// Make selection
+	int crown_player_id = game.get_crown();
+	game.make_selection( crown_player_id, new vector( 1, 3, 4 ) );
+
+	// Make votes
+	for( int i=0; i<5; i++ )
+	{
+		game.add_selection_vote( players[ i ].id, true );
+	}
+
 	// Main loop
 	while( !s3eDeviceCheckQuitRequest() )
 	{
